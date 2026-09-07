e3be844c41f0700570b9fec2509d607bf9cf390f
GesPm <orges.berisha2023@gmail.com>
2026-09-07 19:58:48 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/orionhl-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+app.command("/orionhl-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/orionhl-ping - Check bot latency
+/orionhl-catfact - Get a cat fact`
+  });
+});
+
+app.command("/orionhl-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+})
+
+;(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
